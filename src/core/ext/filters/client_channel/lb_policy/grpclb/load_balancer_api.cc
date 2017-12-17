d9837e1c0a57a79fa89ec347308ebe3ec00de74e
Zhongming Qu <qzmfranklin@gmail.com>
2017-12-16 19:51:05 -0800

    Rename #include "third_party/nanopb{=>/cc}*"
    The command used to do it:
    
        git grep -l '#include "third_party/nanopb/' | xargs sed -i 's|#include
    "third_party/nanopb/|#include "third_party/cc/nanopb/|g'

--- src/core/ext/filters/client_channel/lb_policy/grpclb/load_balancer_api.cc
+++ src/core/ext/filters/client_channel/lb_policy/grpclb/load_balancer_api.cc
@@ -17,8 +17,8 @@
  */
 
 #include "src/core/ext/filters/client_channel/lb_policy/grpclb/load_balancer_api.h"
-#include "third_party/nanopb/pb_decode.h"
-#include "third_party/nanopb/pb_encode.h"
+#include "third_party/cc/nanopb/pb_decode.h"
+#include "third_party/cc/nanopb/pb_encode.h"
 
 #include <grpc/support/alloc.h>
 
